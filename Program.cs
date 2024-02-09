1b82e0a383b47e1bf806fc8e2ffef40a9ce0c69c
Aristotelis Ntouros <ar.ntouros@kaizengaming.com>
2024-02-09 21:16:46 +0200

    Add existing project files to Git

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using MessageHandlerService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Map SignalR hub
+app.MapHub<MessageHub>("/messageHub");
+
+app.MapControllers();
+
+app.Run();
---
+++ Startup.cs
@@ -0,0 +1,36 @@
+using MessageHandlerService.Hubs;
+using Microsoft.OpenApi.Models;
+
+namespace MessageHandlerService;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSignalR();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Message Handler API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Message Handler API V1");
+            });
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<MessageHub>("/messageHub");
+            });
+        }
+    }
